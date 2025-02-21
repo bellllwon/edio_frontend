a63dd87fa4a8f75e42b4edb732b18c56070c125f
bellllwon <bellwon95@gmail.com>
2025-02-21 19:37:04 +0900

    add: storybook 설정 변경
    - preview에 공통 decorator 적용
    - 컴포넌트 사용을 위해 tsx로 변경
    - page 렌더링을 위해 rsc 설정 변경

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,10 @@
+import React from "react"
 import type { Preview } from "@storybook/react"
 import "../app/globals.css"
 import { initialize, mswLoader } from "msw-storybook-addon"
+import MSWProvider from "../src/shared/MswProvider"
+import QueryProvider from "../src/shared/QueryProvider"
+import handlers from "../mocks/handlers"
 
 initialize()
 
@@ -15,8 +19,18 @@ const preview: Preview = {
     nextjs: {
       appDirectory: true,
     },
+    msw: { handlers },
   },
   loaders: [mswLoader],
+  decorators: [
+    (Story) => (
+      <MSWProvider>
+        <QueryProvider>
+          <Story />
+        </QueryProvider>
+      </MSWProvider>
+    ),
+  ],
 }
 
 export default preview
