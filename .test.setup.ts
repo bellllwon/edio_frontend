3ce20c63d0274ec333e7d273d5a674e514cd70e4
bellllwon <bellwon95@gmail.com>
2025-02-24 23:46:23 +0900

    add: 라이브러리 추가 및 환경 설정
    - react hook form,
    - testing-library/jest-dom
    - shadcn form
    - tsconfig 수정

--- .test.setup.ts
+++ .test.setup.ts
@@ -1,5 +1,6 @@
 import { afterAll, afterEach, beforeAll } from "vitest"
 import { server } from "./mocks/server"
+import "@testing-library/jest-dom"
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
