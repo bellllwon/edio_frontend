32ad4cd29425b3d1e2228baac8516c6acd76ff68
Kyeongho <dagh1218@naver.com>
2025-03-19 17:04:37 +0900

    add: #54 워크스페이스 페이지 내 컴포넌트에 폴더 내 데이터 조회 API 연동
    - root 기준 하위 디렉터리 및 덱 조회 API 연동
    - 하위 디렉터리의 경우 상위 폴더 / 현재 폴더 형식으로 header 구성

--- src/folder/FolderDetailContainer.tsx
+++ src/folder/FolderDetailContainer.tsx
@@ -1,149 +1,44 @@
-import { Folder } from "@/src/folder/api"
+"use client"
+
+import { Folder, getAllFolders } from "@/src/folder/api"
 import FolderDetailSection from "@/src/folder/FolderDetailSection"
-
-const folderDetails: Folder[] = [
-  {
-    id: 1,
-    name: "Default",
-    subFolders: [],
-    decks: [
-      {
-        id: 1,
-        name: "Deck 1",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Dir 1",
-    subFolders: [],
-    decks: [
-      {
-        id: 2,
-        name: "Deck 2",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 3,
-        name: "Deck 3",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 4,
-        name: "Deck 4",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Dir 2",
-    subFolders: [],
-    decks: [
-      {
-        id: 5,
-        name: "Deck 5",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 6,
-        name: "Deck 6",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 7,
-        name: "Deck 7",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 8,
-        name: "Deck 8",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 9,
-        name: "Deck 9",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-      {
-        id: 10,
-        name: "Deck 10",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Dir 4",
-    subFolders: [
-      {
-        id: 5,
-        name: "Dir 5",
-        subFolders: [],
-        decks: [
-          {
-            id: 12,
-            name: "Deck 12",
-            description: "ttt",
-            isShared: false,
-            isFavorite: false,
-          },
-        ],
-      },
-    ],
-    decks: [
-      {
-        id: 11,
-        name: "Deck 11",
-        description: "ttt",
-        isShared: false,
-        isFavorite: false,
-      },
-    ],
-  },
-]
-
-function flatFolders(folders: Folder[], flattedFolders: Folder[]): void {
-  folders.forEach((folder) => {
-    flattedFolders.push(folder)
-    if (folder.subFolders.length > 0) {
-      let subFolders: Folder[] = folder.subFolders.map((sub) => {
-        sub.name = `${folder.name} / ${sub.name}`
-        return sub
-      })
-      flatFolders(subFolders, flattedFolders)
-    }
-  })
-}
+import { useQuery } from "@tanstack/react-query"
+import { useEffect, useState } from "react"
 
 export default function FolderDetailContainer() {
-  const flattedFolder: Folder[] = []
-  flatFolders(folderDetails, flattedFolder)
+  const { data } = useQuery(getAllFolders())
+  const [displayFolders, setDisplayFolders] = useState<Folder[]>([])
+
+  // 하위 폴더 내 데이터들을 flat 하게 펼치기 위한 구성
+  useEffect(() => {
+    const flatFolders = (folders: Folder[], flattedFolders: Folder[]): void => {
+      folders.forEach((folder) => {
+        flattedFolders.push(folder)
+        if (folder.subFolders.length > 0) {
+          let subFolders: Folder[] = folder.subFolders.map((sub) => {
+            return {
+              id: sub.id,
+              name: `${folder.name} / ${sub.name}`,
+              decks: sub.decks,
+              subFolders: [],
+            }
+          })
+          flatFolders(subFolders, flattedFolders)
+        }
+      })
+    }
+
+    let flattedFolders: Folder[] = []
+    if (data !== undefined) {
+      flattedFolders.push(data)
+      flatFolders(data.subFolders, flattedFolders)
+      setDisplayFolders(flattedFolders)
+    }
+  }, [data])
 
   return (
     <div className="relative overflow-x-hidden p-12">
-      {flattedFolder.map((folderDetail) => (
+      {displayFolders.map((folderDetail) => (
         <FolderDetailSection
           key={folderDetail.id}
           folderDetail={folderDetail}
